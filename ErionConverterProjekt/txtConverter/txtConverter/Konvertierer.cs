bde3ff1c507d27f10dd5d692d78460ee0a0c43fb
DeonisAjeti <78471573+DeonisAjeti@users.noreply.github.com>
2025-04-03 19:22:04 +0200

    VsCode Datei erstellt + Beispieltxt

---
+++ ErionConverterProjekt/txtConverter/txtConverter/Konvertierer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace txtConverter
+{
+    public partial class Konvertierer : Form
+    {
+        public Konvertierer()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Title = "Datei auswählen";
+                ofd.Filter = "Textdateien (*.txt)|*.txt|Alle Dateien (*.*)|*.*";
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = ofd.FileName;
+                    string content = File.ReadAllText(filePath); // Lies den Inhalt der Datei ein
+                    MessageBox.Show(content); // Beispielausgabe
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
